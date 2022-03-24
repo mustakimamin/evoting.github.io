37ce52d4138ce068a57871f407cb5beb2569ae32
mustakimamin <102291924+mustakimamin@users.noreply.github.com>
2022-03-24 05:47:36 -0700

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -75,7 +75,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
+	'hostname' => 'evoting.github.io',
 	'username' => 'root',
 	'password' => '',
 	'database' => 'db_evoting',
